39feb14f91057c7d51fe60ec9506a062dc018b94
jopwork <jopwork@gmail.com>
2018-02-04 10:00:52 +0800

    Ignored elixir_ls director.

--- test/elixir_lang_test.exs
+++ test/elixir_lang_test.exs
@@ -5,4 +5,14 @@ defmodule ElixirLangTests do
   test "addition and equality" do
     assert 3 == 1+2
   end
+
+  test "division operator returns float" do
+    assert 3 == 6 / 2
+    assert 3.0 === 6 / 2
+  end
+
+  test "div returns integer" do
+      assert 3 == div(6,2)
+      assert 3 === div(6,2)
+  end
 end
