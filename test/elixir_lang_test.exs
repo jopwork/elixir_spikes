d26ebcdff489436c7cf48804adeaa7744ded7573
jopwork <jopwork@gmail.com>
2018-02-04 09:22:35 +0800

    Created elixir_lang_test

---
+++ test/elixir_lang_test.exs
@@ -0,0 +1,13 @@
+defmodule ElixirLangTests do
+  use ExUnit.Case
+  doctest ElixirSpikes
+
+  test "greets the world" do
+    assert ElixirSpikes.hello() == :world
+  end
+
+  test "another test case" do
+    IO.puts "Another Test Case"
+    assert 1 = 1
+  end
+end
