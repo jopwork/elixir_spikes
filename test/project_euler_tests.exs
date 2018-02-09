19b72ca1ac5ffaac8981048b1c514f0b320b54d2
jopwork <jopwork@gmail.com>
2018-02-10 00:20:58 +0800

    Project Euler Problem 1 solution

--- test/project_euler_tests.exs
+++ test/project_euler_tests.exs
@@ -6,10 +6,10 @@ defmodule ProjectEulerTests do
   """
 
   test "Problem 1: Multiple of 3 and 5" do
-    mult = fn n -> rem(n, 3) == 0 or rem(n, 5) == 0 end
+    mult? = fn n -> rem(n, 3) == 0 or rem(n, 5) == 0 end
     answer = 
       1..999
-      |> Enum.filter(mult)
+      |> Enum.filter(mult?)
       |> Enum.sum()
     IO.puts("Problem 1: #{answer}")
   end
