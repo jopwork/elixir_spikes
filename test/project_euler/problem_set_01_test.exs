4293e6de0cb70c28b9468161417fd4e72ebdd5b1
jopwork <jopwork@gmail.com>
2018-02-13 22:47:55 +0800

    Problem 6 implementation

--- test/project_euler/problem_set_01_test.exs
+++ test/project_euler/problem_set_01_test.exs
@@ -79,4 +79,27 @@ defmodule ProblemSet01Tests do
   test "Problem 5: smallest multiple" do
     IO.inspect(lcm(1..20), label: "Problem 5")
   end
+
+  @doc """
+  The sum of the squares of the first ten natural numbers is,
+
+    1^2 + 2^2 + ... + 10^2 = 385
+
+  The square of the sum of the first ten natural numbers is,
+
+    (1 + 2 + ... + 10)^2 = 552 = 3025
+
+  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+  """
+  test "Problem 6: " do
+    count = 100
+
+    sum_of_squares = 1..count |> Enum.map(& &1*&1) |> Enum.reduce(&+/2)
+    sum = 1..count |> Enum.reduce(&+/2) 
+    square_of_sum = sum*sum
+
+    IO.inspect(square_of_sum - sum_of_squares, label: "Problem 6")
+  end
 end
