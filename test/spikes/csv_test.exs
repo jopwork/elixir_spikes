607ddfcfd001326e7d743752189775b6aefca166
Jop Pascual <jopwork@gmail.com>
2018-05-13 13:00:25 +0800

    Added read_csv_file function for reading a csv file and returning the rows

--- test/spikes/csv_test.exs
+++ test/spikes/csv_test.exs
@@ -6,10 +6,28 @@ defmodule CsvTests do
     assert File.exists?(filename)
 
     File.stream!(filename)
-    |> IO.inspect()
     |> CSV.decode()
-    |> Stream.take(1)
     |> Enum.to_list()
     |> IO.inspect()
+  end
+
+  test "file does not exist" do
+    {:error, "File does not exist"} = read_csv_file("unknownfile.xxx")
+    {:ok, rows} = read_csv_file("test/spikes/MTXXX.csv")
+
+    IO.inspect rows
+  end
+
+  def read_csv_file(filename) do
+    if ! File.exists?(filename) do
+      {:error, "File does not exist"}
+    else
+      rows =
+        File.stream!(filename)
+        |>CSV.decode()
+        |>Enum.to_list()
+
+      {:ok, rows}
+    end
   end
 end
